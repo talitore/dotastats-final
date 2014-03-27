8bdd064a189bda137136d5717bbd4434377b419a
Taylor Kems <tekems@gmail.com>
2014-03-27 18:42:55 -0500

    Rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DotastatsFinal::Application.config.secret_key_base = 'ad98e70a33ba6d8008df1581953a878d51d41d161271adbd3e5efb470d05c30e2fd902ae0a553b8f627aef9c68be4a7a2c77bda452618be880e5c94ff066f0b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DotastatsFinal::Application.config.session_store :cookie_store, key: '_dotastats-final_session'
